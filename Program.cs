78466a9012fde084a06542c9aef7449cf0c668bd
ANH KHOA <ANH KHOA@LOQ>
2026-04-10 15:10:39 +0700

    switch to SQLite for Render deployment

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace LibaryManagement
 
             // Add services to the container.
             builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers().AddJsonOptions(x =>
                 x.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
